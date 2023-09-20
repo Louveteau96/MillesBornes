cc5733dd27a82450c3cff9917f758eb2e4b2ee79
Louveteau96 <114427182+Louveteau96@users.noreply.github.com>
2023-09-20 09:43:40 +0200

    Exercice d à faire

--- src/jeu/Sabot.java
+++ src/jeu/Sabot.java
@@ -2,12 +2,40 @@ package jeu;
 import cartes.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Sabot {
-	private ArrayList<Carte> jeuDeCartes;
+	private Carte [] tableauDeCartes;
+	private int nbCartes;
 
 	public Sabot() {
-		this.jeuDeCartes = new ArrayList<Carte>(110);
+		this.tableauDeCartes = new Carte[110];
+	}
+	
+	public boolean estVide() {
+		return nbCartes ==0;
+	}
+	
+	private void ajouterCarte(Carte carte) {
+		if(nbCartes ==110) {
+			throw new ArrayIndexOutOfBoundsException();
+		}else {
+			tableauDeCartes[nbCartes] = carte;
+			nbCartes++;
+		}
+	}
+	
+	//C'est une surcharge avec les plusieurs cartes
+	public void ajouterFamilleCarte(Carte carte) {
+		int nbrFamille = carte.getNombre();
+		for (int i = 0; i < nbrFamille; i++) {
+			ajouterCarte(carte);
+		}
+	}
+	
+	//Varargs
+	public void ajouterFamilleCarte(Carte... args) {
+		
 	}
 
 }
